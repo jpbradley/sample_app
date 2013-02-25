f8539c5c9aa9da36d1b2b92f4b43bdfe1aa75ffc
Jason Bradley <jason.p.bradley@gmail.com>
2013-02-24 19:04:00 -0600

    commit changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,6 +42,16 @@ describe User do
 		end
 	end
 
+	describe "with a lower-case email" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+
 	describe "with a password that's too short" do
 		before { @user.password = @user.password_confirmation = "a" * 5 }
 		it { should_not be_valid }
